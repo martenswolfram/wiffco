9a29049d3fb73a5a4fbf697ed4000b63d2e6a57c
Wolfram Martens <w.martens@tudelft.nl>
2025-10-17 16:05:07 +0200

    Add example script that uses test data

---
+++ example_script.py
@@ -0,0 +1,37 @@
+import pathlib
+from twain_wifco.config import (
+    control_evaluation_system_from_json,
+    control_optimization_from_json,
+    statistics_from_json)
+
+# Config folder path
+config_folder = pathlib.Path(__file__).parent / "tests/data/"
+    
+# Control evaluation System
+json_path = config_folder / "control_evaluation_system.json"
+control_evaluation_system = control_evaluation_system_from_json(json_path=json_path)
+
+# Ambient conditions
+json_path = config_folder / "discrete_ambient_statistics.json"
+ambient_statistics = statistics_from_json(json_path=json_path)
+
+# Evaluation period
+duration = 20
+
+for file_name in ["grid_search_optimization.json",
+                  "simultaneous_optimization.json",
+                  "lagrangian_relaxation.json"]:
+    json_path = config_folder / file_name
+    optimization = control_optimization_from_json(json_path=json_path)
+    
+    # Optimization
+    optimal_policy = optimization.optimize_policy(
+        control_eval_system=control_evaluation_system,
+        ambient_condition_statistics=ambient_statistics,
+        duration=duration)
+
+    # output
+
+    print(f"\nOptimal control set points for file '{file_name}':")
+    print(optimal_policy.control_setpoints.T)
+
